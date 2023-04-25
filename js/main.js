1d70545ff3fdfb89e571a87760f000f2ef97ea38
vinith <vinithdevadiga7@.com>
2023-04-25 16:14:12 +0530

    navbar breadcrump added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-const toggle = document.getElementById("breadcrump-btn")
+const toggleBtn = document.getElementById("breadcrump-btn");
+const sortDownBtn = document.getElementById("sort-down-btn");
+const togglemenuContainer = document.getElementById("toggle-menu-container");
+const cleanSericesContainer = document.getElementById("clean-serivces-expand");
+
+function toggle() {
+  const icon = this.querySelector("i");
+  icon.classList.toggle("fa-close");
+  togglemenuContainer.classList.toggle("hidden");
+}
+function toggleSort() {
+  this.classList.toggle("fa-sort-up");
+  cleanSericesContainer.classList.toggle("hidden");
+}
+
+toggleBtn.addEventListener("click", toggle);
+sortDownBtn.addEventListener("click", toggleSort);
