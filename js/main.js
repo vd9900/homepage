2ef3a762445c77e12f76eeb7844cf1b42ea6648a
vinith <vinithdevadiga7@.com>
2023-04-30 16:24:22 +0530

    tabbed content section

--- js/main.js
+++ js/main.js
@@ -42,7 +42,6 @@ function toggle() {
   icon.classList.toggle("fa-close");
   togglemenuContainer.classList.toggle("hidden");
 }
-alert("hello");
 function toggleSort() {
   this.classList.toggle("fa-sort-up");
   // this.style.transform = `scale(-1)`;
